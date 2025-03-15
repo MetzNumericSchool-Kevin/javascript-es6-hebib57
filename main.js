08b3c51c8f64360c8464398aa53cdebe7f933cc0
hebib57 <d-del57000@hotmail.fr>
2025-03-15 21:59:41 +0100

    Exercice 6

--- main.js
+++ main.js
@@ -4,19 +4,23 @@
  */
 
 // Exercice 1.1 : Transforme la concaténation en interpolation de chaîne de caractères avec les backticks `
-function afficheAddresse(address) {
-  return (
-    "Votre adresse est : " +
-    address.street +
-    ", " +
-    address.city +
-    ", " +
-    address.country +
-    "."
-  );
-}
+// function afficheAddresse(address) {
+//   return (
+//     "Votre adresse est : " +
+//     address.street +
+//     ", " +
+//     address.city +
+//     ", " +
+//     address.country +
+//     "."
+//   );
+// }
 
 console.log("Exercice 1.1");
+
+function afficheAddresse(address) {
+  return `Votre Adresse est : ${address.street}, ${address.city}, ${address.country}.`;
+}
 console.log(
   afficheAddresse({
     street: "10 rue de la republique",
@@ -27,9 +31,15 @@ console.log(
 
 // Exercice 1.2 : Utilise les chaînes multilignes avec les backticks ` pour afficher plus simplement le menu
 console.log("Exercice 1.2");
-console.log(
-  "Menu de la semaine :\n    - Pâte à tartine\n    - Hoagie\n    - Salade de pommes"
-);
+console.log(`
+Menu de la semaine :
+    - Pâte à tartine
+    - Hoagie
+    - Salade de pommes
+`);
+// console.log(
+//   "Menu de la semaine :\n    - Pâte à tartine\n    - Hoagie\n    - Salade de pommes"
+// );
 
 /**
  * Exercice 2
@@ -39,19 +49,23 @@ console.log(
 const colors = ["Red", "Green", "Blue", "Yellow"];
 
 // Exercice 2.1 : Utilise la décomposition pour créer les constantes de couleurs plutôt que d'accéder aux indices
-const red = colors[0];
-const green = colors[1];
-const blue = colors[2];
-const yellow = colors[3];
+// const red = colors[0];
+// const green = colors[1];
+// const blue = colors[2];
+// const yellow = colors[3];
+
+const [red, green, blue, yellow] = colors;
 
 console.log("Exercice 2.1");
 console.log(red, green, blue, yellow);
 
 // Exercice 2.2 : La même chose, mais en ignorant ici "Red" et "Yellow"
-const green2 = colors[1];
-const blue2 = colors[2];
+// const green2 = colors[1];
+// const blue2 = colors[2];
 
 console.log("Exercice 2.2");
+
+const [, green2, blue2] = colors;
 console.log(green2, blue2);
 
 // ------ Version objet
@@ -79,21 +93,28 @@ const roman = {
 };
 
 // Exercices 2.3 : Décompose l'objet pour récupérer le titre, l'éditeur et le tome dans des constantes
-const title = roman.title;
-const editor = roman.editor;
-const tome = roman.tome;
+// const title = roman.title;
+// const editor = roman.editor;
+// const tome = roman.tome;
 
 console.log("Exercice 2.3");
+const { title, editor, tome } = roman;
 console.log(title, editor, tome);
 
 // Exercice 2.4 : Décompose l'objet pour récupérer le nom du personnage principal,
 // le nom du premier personnage secondaire
 // et la voix Japonaise dans l'anime du second personnage secondaire
-const mainCharacterName = roman.mainCharacter.name;
-const secondCharacterName = roman.characters[0].name;
-const thirdCharacterJapaneseVoice = roman.characters[1].animeJapaneseVoice;
+// const mainCharacterName = roman.mainCharacter.name;
+// const secondCharacterName = roman.characters[0].name;
+// const thirdCharacterJapaneseVoice = roman.characters[1].animeJapaneseVoice;
 
 console.log("Exercice 2.4");
+const {
+  mainCharacter: { name: mainCharacterName },
+} = roman;
+const { name: secondCharacterName } = roman.characters[0];
+const { animeJapaneseVoice: thirdCharacterJapaneseVoice } = roman.characters[1];
+
 console.log(
   mainCharacterName,
   secondCharacterName,
@@ -107,11 +128,13 @@ let lastname = "Doe";
 
 // Exercice 2.5 : Faites l'inversion du prénom et nom avec la décomposition
 // plutôt qu'avoir une variable temporaire
-let temp = firstname;
-firstname = lastname;
-lastname = temp;
+// let temp = firstname;
+// firstname = lastname;
+// lastname = temp;
 
 console.log("Exercice 2.5");
+[firstname, lastname] = [lastname, firstname];
+
 console.log(firstname, lastname);
 
 // ------
@@ -123,11 +146,14 @@ const person = {
 };
 
 // Exercice 2.6 : Fais une décomposition du paramètre pour extraire directement le prénom et le nom
-function hello(person) {
-  console.log(`Bonjour ${person.firstname} ${person.lastname} !`);
-}
+// function hello(person) {
+//   console.log(`Bonjour ${person.firstname} ${person.lastname} !`);
+// }
 
 console.log("Exercice 2.6");
+function hello({ firstname, lastname }) {
+  console.log(`Bonjour ${firstname} ${lastname} !`);
+}
 hello(person);
 
 /**
@@ -136,24 +162,27 @@ hello(person);
  */
 
 // Exercice 3.1 : Décompose le tableau colors pour ne garder dans le reste que les couleurs bleue et jaune
-const restOfColors = [];
+// const restOfColors = [];
 
-restOfColors.push(colors[2]);
-restOfColors.push(colors[3]);
+// restOfColors.push(colors[2]);
+// restOfColors.push(colors[3]);
 
 console.log("Exercice 3.1");
+const restOfColors = [blue, yellow];
 console.log(restOfColors);
 
 // Exercice 3.2 : Décompose l'objet, de telle sorte à ce que le reste contienne seulement les clés
 // titre, editeur, tome, titre, auteur
 
-const roman2 = {};
-roman2.title = roman.title;
-roman2.editor = roman.editor;
-roman2.tome = roman.tome;
-roman2.auteur = roman.auteur;
+// const roman2 = {};
+// roman2.title = roman.title;
+// roman2.editor = roman.editor;
+// roman2.tome = roman.tome;
+// roman2.auteur = roman.auteur;
 
 console.log("Exercice 3.2");
+const roman2 = { ...roman };
+
 console.log(roman2);
 
 // ------
@@ -162,15 +191,20 @@ const randomNumbers = [12, 13, 52, 68, 2, 23, 5];
 // Exercice 3.3 : Math.min prend un nombre indéfini d'arguments
 // Utilise l'opérateur de reste pour pouvoir passer un tableau
 console.log("Exercice 3.3");
-console.log(Math.min(12, 13, 52, 68, 2, 23, 5));
+console.log(Math.min(...randomNumbers));
+// console.log(Math.min(12, 13, 52, 68, 2, 23, 5));
 
 // Exercice 3.4 : Transforme cette fonction pour qu'elle puisse prendre
 // un nombre indéfini de paramètres car actuellement elle en prend que 3
-function min(a, b, c) {
-  return Math.min(a, b, c);
-}
+// function min(a, b, c) {
+//   return Math.min(a, b, c);
+// }
 
 console.log("Exercice 3.4");
+function min(...args) {
+  return Math.min(...args);
+}
+
 console.log(min(12, 13, 52, 68, 2, 23, 5)); // Le résultat doit être 2
 
 /**
@@ -179,17 +213,20 @@ console.log(min(12, 13, 52, 68, 2, 23, 5)); // Le résultat doit être 2
  */
 
 // Exercice 4.1 : Utilise l'opérateur de dispersion pour ajouter "Pink" et "Magenta" dans un nouveau tableau
-const moreColors = colors.concat("Pink", "Magenta");
+// const moreColors = colors.concat("Pink", "Magenta");
 
 console.log("Exercice 4.1");
+const moreColors = [...colors, "Pink", "Magenta"];
+
 console.log(moreColors);
 
 // Exercice 4.2 : Utilise l'opérateur de dispersion pour créer un nouveau tableau
 // où Red est ajouté au début, et "Violet" à la fin
 const partialRainbow = ["Orange", "Yellow", "Green", "Blue", "Indigo"];
-const rainbow = ["Red"].concat(partialRainbow, ["Violet"]);
+// const rainbow = ["Red"].concat(partialRainbow, ["Violet"]);
 
 console.log("Exercice 4.2");
+const rainbow = ["Red", ...partialRainbow, "Violet"];
 console.log(rainbow);
 
 // Exercice 4.3 : Copier l'adresse en ajoutant une nouvelle clé pour le code postal
@@ -199,15 +236,18 @@ const adress = {
   country: "France",
 };
 
-const completeAdress = Object.assign({}, adress, { postalCode: 75001 });
+// const completeAdress = Object.assign({}, adress, { postalCode: 75001 });
 
 console.log("Exercice 4.3");
+const completeAdress = { ...adress, postalCode: 75001 };
+
 console.log(completeAdress);
 
 // Exercice 4.4 : Utilise l'opérateur de dispersion pour copier l'objet person et modifier la valeur de l'age à 20
-const person2 = Object.assign({}, person, { age: 20 });
+// const person2 = Object.assign({}, person, { age: 20 });
 
 console.log("Exercice 4.4");
+const person2 = { ...person, age: 20 };
 console.log(person2);
 
 /**
@@ -216,34 +256,42 @@ console.log(person2);
  */
 
 // Exercice 5.1 : Convertir en fonction fléchée
-const thatsAllFolks = function () {
-  console.log("That's all folks!");
-};
+// const thatsAllFolks = function () {
+//   console.log("That's all folks!");
+// };
 
 console.log("Exercice 5.1");
+const thatsAllFolks = () => {
+  console.log("That's all folks!");
+};
 thatsAllFolks();
 
 // Exercice 5.2 : Convertir toutes les fonctions en fonction fléchée
 // Et si c'est possible, faire un return implicite
-const makeDouble = function (numbers) {
-  return numbers.map(function (number) {
-    const newNumber = number * 2;
-    return newNumber;
-  });
-};
+// const makeDouble = function (numbers) {
+//   return numbers.map(function (number) {
+//     const newNumber = number * 2;
+//     return newNumber;
+//   });
+// };
 
 console.log("Exercice 5.2");
+const makeDouble = (numbers) => numbers.map((number) => number * 2);
 console.log(makeDouble([1, 2, 3]));
 
 // Exercice 5.3 : Convertir en fonction fléchée avec un return implicite
-function convertToPerson(firstnameLastnameTuple) {
-  return {
-    firstname: firstnameLastnameTuple[0],
-    lastname: firstnameLastnameTuple[1],
-  };
-}
+// function convertToPerson(firstnameLastnameTuple) {
+//   return {
+//     firstname: firstnameLastnameTuple[0],
+//     lastname: firstnameLastnameTuple[1],
+//   };
+// }
 
 console.log("Exercice 5.3");
+const convertToPerson = (firstnameLastnameTuple) => ({
+  firstname: firstnameLastnameTuple[0],
+  lastname: firstnameLastnameTuple[1],
+});
 console.log(convertToPerson(["John", "Doe"]));
 
 /**
@@ -253,14 +301,18 @@ console.log(convertToPerson(["John", "Doe"]));
 
 // Exercice 6 : raccourci l'assignation à l'objet
 
-function convertToPerson2([firstname, lastname]) {
-  return {
-    firstname: firstname,
-    lastname: lastname,
-  };
-}
+// function convertToPerson2([firstname, lastname]) {
+//   return {
+//     firstname: firstname,
+//     lastname: lastname,
+//   };
+// }
 
 console.log("Exercice 6");
+const convertToPerson2 = ([firstname, lastname]) => ({
+  firstname,
+  lastname,
+});
 console.log(convertToPerson2(["John", "Doe"]));
 
 /**
